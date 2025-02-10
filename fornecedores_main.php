3decb2089cf06ee87bea8c99f633adebe0d85b74
Willkingblues <87676653+Willkingblues@users.noreply.github.com>
2025-02-10 12:09:03 -0300

    http://localhost/financeiro/fornecedores_main.php
    integração  de tabelas  do  banco de  dados  com, php.
    pagina  com  resultado... em  melhorias.

--- fornecedores_main.php
+++ fornecedores_main.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="Pt-br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,59 +7,40 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-    Include 'conexao.php';
-    ?>
-    <a href="incluir_fornecedores.php" class="btn btn-primary"> Novo Fornecedor</a>
-    <br><br>
-    <table class="table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">id</th>
-        <th scope="col">Nome fornecedor</th>
-        <th scope="col">editar</th>
-        <th scope="col">excluir</th>
-      </tr>
-    </thead>
-    <tbody>
-        <?php
-        $sql = "select * from fornecedores";
-        $busca_fornecedor = mysqli_query($conexao,$sql);
-        while ($array = mysqli_fetch_array($busca_fornecedor)) {
-            $id_fornecedor = $array['id_fornecedor'];
-            $nome_fornecedor = $array['nome_fornecedor'];
-        
-        ?>
-      <tr>
-        <th <?php echo($id_fornecedor);?>></th>
-        <td><?php echo($nome_fornecedor);?></td>
-        <td><button type="button" class="btn btn-primary">Editar</button> </td>
-        <td><button type="button" class="btn btn-primary">Excluir</button> </td>
-      </tr>
-      <?php
-        }
-        ?>
-
-      <tr>
-        <td scope="row">2</td>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>
 <?php
-
-
-
-
+    include 'conexao.php'; // Inclui o arquivo de conexão
+?>    
+<a href="incluir_fornecedores.php" class="btn btn-primary">Novo Fornecedor</a>
+<br><br>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nome Fornecedor</th>
+      <th scope="col">Editar</th>
+      <th scope="col">Excluir</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+        $sql = "SELECT * FROM fornecedores"; // Consulta a tabela
+        $stmt = $pdo->query($sql); // Executa a consulta usando PDO
+        // Laço para trazer os dados da consulta
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $id_fornecedor = $row['id_fornecedor'];
+            $nome_fornecedor = $row['nome_fornecedor'];
+   ?>
+    <tr>
+      <td><?php echo htmlspecialchars($id_fornecedor); ?></td>
+      <td><?php echo htmlspecialchars($nome_fornecedor); ?></td>
+      <td><button type="button" class="btn btn-primary">Editar</button></td>
+      <td><button type="button" class="btn btn-danger">Excluir</button></td>
+    </tr>
+    <?php
+        }
     ?>
-    
+  </tbody>
+</table>
+   
 </body>
 </html>
--- incluir_fornecedores.php
+++ incluir_fornecedores.php
@@ -1,54 +1,82 @@
 <!-- incluir_fornecedores.php -->
-<!DOCTYPE html>
+<!-- incluir_fornecedores.php -->
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fornecedores - Inclusão</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
 </head>
 <body>
-    <form action="_incluir_fornecedores.php" method="post">
+    <div class="container">
+    <form action="processa_incluir_fornecedores.php" method="post">
         <label for="cpfcnpj">CPF/CNPJ</label>
-        <input type="text" id="cpf_cnpj" name="cpf_cnpj" class="form-control"
-        placeholder="Entre com o CPF/CNPJ" required>        
-        <label for="nomefornecedor">Nome Fornecedor</label>
-        <input type="text" id="nomefornecedor"
-        name="nomefornecedor" class="form-control" placeholder="Entre com o nome"
-        required>
+        <input type="text" id="cpf_cnpj" name="cpf_cnpj" class="form-control cpfOuCnpj"
+        placeholder="Entre com o CPF/CNPJ"  required>
+       
+        <label for="nome_fornecedor">Nome Fornecedor</label>
+        <input type="text" id="nome_fornecedor" name="nome_fornecedor" class="form-control"
+        placeholder="Entre com o nome" required>
+ 
         <label for="celular">Celular</label>
         <input type="text" id="celular" name="celular" class="form-control"
         placeholder="Entre com o Celular">
-        <label for="celular">E-mail</label>
+ 
+        <label for="email">E-mail</label>
         <input type="text" id="email" name="email" class="form-control"
         placeholder="Entre com o E-mail">    
+ 
         <label for="cep">CEP</label>
         <input type="text" id="cep" name="cep" class="form-control"
         placeholder="Entre com o CEP">
-        <label for="logradouto">Endereço</label>
+ 
+        <label for="logradouro">Endereço</label>
         <input type="text" id="logradouro" name="logradouro" class="form-control"
         placeholder="Entre com o Endereço">
-        <label for="numero">numero</label>
+ 
+        <label for="numero">Número</label>
         <input type="text" id="numero" name="numero" class="form-control"
         placeholder="Entre com o Número">
+ 
         <label for="complemento">Complemento</label>
         <input type="text" id="complemento" name="complemento" class="form-control"
         placeholder="Entre com o Complemento">
-        <label for="complemento">Cidade</label>    
+
+        <label for="bairro">bairro</label>
+        <input type="text" id="bairro" name="bairro" class="form-control"
+        placeholder="Entre com o bairro">
+ 
+        <label for="cidade">Cidade</label>    
         <input type="text" id="cidade" name="cidade" class="form-control"
         placeholder="Entre com a Cidade">
+ 
         <label for="estado">Estado</label>
         <input type="text" id="estado" name="estado" class="form-control"
         placeholder="Entre com o Estado">
+ 
         <label for="contato">Nome do Contato</label>
         <input type="text" id="contato" name="contato" class="form-control"
         placeholder="Entre com o nome do Contato">                                          
  
         <button type="submit" id="botao" class="btn btn-primary">Incluir</button>        
     </form>
+    </div>
+    <!-- Carregando bibliotecas corretamente -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.min.js">
  
+    </script>
+ 
+    <script type="text/javascript">
+        var options = {
+    onKeyPress: function (cpf, ev, el, op) {
+        var masks = ['000.000.000-000', '00.000.000/0000-00'];
+        $('.cpfOuCnpj').mask((cpf.length > 14) ? masks[1] : masks[0], op);
+        }
+    }
+ 
+    $('.cpfOuCnpj').length > 11 ? $('.cpfOuCnpj').mask('00.000.000/0000-00', options) : $('.cpfOuCnpj').mask('000.000.000-00#', options);
+    </script>
+   
 </body>
 </html>
